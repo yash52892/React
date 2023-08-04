7c5998af1744a9cb45c8954c8ea1b7da5b8af54e
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-04 22:49:02 +0530

    Components in loop
    Using Map

--- App.js
+++ App.js
@@ -6,7 +6,13 @@ function App() {
   return (
     <div>
     <h2>Let's get started!</h2>
-    <ExpenseItem></ExpenseItem>
+    <div>
+      {
+        Expense.map((Expense) =>(
+        <ExpenseItem E_name={Expense.ExpName}E_price={Expense.price}E_location={Expense.location}></ExpenseItem>
+        ))
+      }
+    </div>
   </div>
   );
 }
