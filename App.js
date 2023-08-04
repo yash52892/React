9e37115fed07611114deecc8cd64b2b8c954d24c
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-04 19:45:50 +0530

    Expense item comp. is attached to the Parent App

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+    <h2>Let's get started!</h2>
+    <ExpenseItem></ExpenseItem>
+  </div>
+  );
+}
+
+export default App;
