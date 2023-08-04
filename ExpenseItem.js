48a675a552c6871e7a0224351fd28658ba12938a
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-04 19:52:00 +0530

    Expense is added

---
+++ ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return(
+        <div>
+        <h2>ExpenseItem!</h2>
+        <h3>Food Rs 10</h3>
+        <h3>Petrol Rs 100</h3>
+        <h3>Movies Rs 200</h3>
+        </div>
+    )
+}
+
+export default ExpenseItem
