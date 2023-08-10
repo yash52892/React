862256dc21ad377fcb279fc2d40384948c2a9b99
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-10 21:01:11 +0530

    Update ExpenseDetails.js

--- ExpenseDetails.js
+++ ExpenseDetails.js
@@ -1,9 +1,14 @@
   const ExpenseDetails = (props)=>{
+    const clickHandler = (propsid) =>{
+      document.getElementById(propsid).remove();
+      
+     }
     return (
       <div className="expense-item__description">
       <h2 className="expense-item">{props.title}</h2>
         <div className="expense-item__price">Rs.{props.amount}</div>
         <div className="expense-item">{props.location}</div>
+        <button className="expense-item" onClick={()=>clickHandler(props.id)}>Delete</button>
       </div>
     )
   }
