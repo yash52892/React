c9e594fcdabef7171e3d68246b4b9d7c36f3c9f4
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-10 22:15:36 +0530

    useState is used to update the values in UI

--- ExpenseDetails.js
+++ ExpenseDetails.js
@@ -1,14 +1,22 @@
-  const ExpenseDetails = (props)=>{
-    const clickHandler = (propsid) =>{
+import React, { useState } from "react";
+
+const ExpenseDetails = (props)=>{
+    const clickHandler2 = (propsid) =>{
       document.getElementById(propsid).remove();
-      
      }
+    const [p,udpate]= useState(props.amount);
+     const clickHandler1 = () =>{
+      udpate('100');
+      console.log(p)
+     };
+
     return (
       <div className="expense-item__description">
       <h2 className="expense-item">{props.title}</h2>
-        <div className="expense-item__price">Rs.{props.amount}</div>
+        <div className="expense-item__price">Rs.{p}</div>
         <div className="expense-item">{props.location}</div>
-        <button className="expense-item" onClick={()=>clickHandler(props.id)}>Delete</button>
+        <button className="expense-item" onClick={()=>clickHandler1()}>Add</button>
+        <button className="expense-item" onClick={()=>clickHandler2(props.id)}>Delete</button>
       </div>
     )
   }
