445525bc81a08d936ba0d8c06c9e4b8474d02d10
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-13 19:35:21 +0530

    Update ExpenseForm.js
    Css is added with useState

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,31 +1,44 @@
+import "./ExpenseForm.css"
+import React, { useState } from "react";    
 const ExpenseForm = () =>{
-    const getData = (form)=> {
-        var formData = new FormData(form);
-        for (var pair of formData.entries()) {
-          console.log(pair[0] + ": " + pair[1]);
-        }
-        console.log(Object.fromEntries(formData));
-      }
-         const el = document.getElementById('form');
-        if (el) 
-        {
-            el.addEventListener("Submit", function (e) {
-                e.preventDefault();
-            getData(e.target);
-            });
-        }
-    return (
-    <div>
-    <form id="form">
-        <label>Expense Title</label>
-        <input type="text"/>
-        <label>Expense Amount</label>
-        <input type="number"/>
-        <label>Expense Date</label>
-        <input type="date"/>
-        <input type="submit" value="Submit"></input>
-    </form>
-    </div>
-    );
+
+    const [t,udpate_t]= useState('');
+    const [a,udpate_a]= useState('');
+    const [d,udpate_d]= useState('');
+
+     const enteredtitle = (event) =>{
+      udpate_t(event.target.value);
+     };
+     
+     const enteredamount = (event) =>{
+      udpate_a(event.target.value);
+     };
+     
+     const entereddate = (event) =>{
+        udpate_d(event.target.value);
+     };
+      
+return(
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label className="new-expense__control label">Expense Title</label>
+                    <input type="text" onChange={enteredtitle} className="new-expense__control input"/>
+                </div>
+                <div className="new-expense__control">
+                    <label className="new-expense__control label">Expense Amount</label>
+                    <input type="number" onChange={enteredamount} className="new-expense__control input"/>
+                </div>
+                <div className="new-expense__control">
+                    <label className="new-expense__control label">Expense Date</label>
+                    <input type="date" onChange={entereddate} className="new-expense__control input"/>
+                </div>
+                <div className="new-expense__actions"> 
+                <button type="submit">Add Expense</button>
+                </div>
+            </div>
+    </form>  
+);
 }
+
 export default ExpenseForm;
