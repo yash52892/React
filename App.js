c21aa6bbc194692ae72fb46871807c1222a5f2c7
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-16 06:49:40 +0530

    Update App.js
    Imported Expense

--- App.js
+++ App.js
@@ -1,39 +1,42 @@
-import ExpenseItem from "./components/Expense/ExpenseItem"
-import New_expense from "./components/New_expense/Newexpense.js";
-import "./components/New_expense/ExpenseForm.css"
+import Expenses from "./components/Expense/Expense";
+import Newexpense from "./components/New_expense/Newexpense";
+import "./components/New_expense/ExpenseForm.css";
+import {useState} from "react";
+import React from "react";
 
 
-const App = () =>{
-  const expenses = [
+const App = (props) =>{
+  let expenses = [
     {
-      id: 'e1',
+      id: 1,
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
       location: 'Mysuru'
     },
     {
-      id: 'e3',
+      id: 2,
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
       location: 'Bengaluru'
     },
     {
-      id: 'e4',
+      id: 3,
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
       location: 'Shimoga'
-    },
+    }
   ];
-  
+  const [newExp, setSelectedItems] = useState(expenses);
+   const add_data = (data)=>{
+    setSelectedItems((newExp) => [...expenses, data])
+    }; 
   return (
-    <div id="List">
-    <New_expense></New_expense>
-    <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location} id={expenses[0].id}></ExpenseItem>
-    <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location} id={expenses[1].id}></ExpenseItem>
-    <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location} id={expenses[2].id}></ExpenseItem>
+    <div>
+    <Newexpense getData={add_data}/>
+    <Expenses items={newExp} />
     </div>
   );
 }
