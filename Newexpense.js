3e791bb45a067d22140253846afc3aa655e0a865
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-08-25 06:11:44 +0530

    Update Newexpense.js
    Conditional rendering

--- Newexpense.js
+++ Newexpense.js
@@ -1,17 +1,30 @@
 import "./Newexpense.css"
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const Newexpense = (props) =>{
+    const [init, updatedState]=useState(true);
     const addFormHandler =(exp)=>{
-        const newExp={
-            id: Math.random(),
-            ...exp  
-        };
-        props.getData(newExp);
+        props.getData(exp);
+        updatedState(true);
     }
+    
+    const onClick=()=>{
+        updatedState(false);
+    }
+    const cancelhandled=()=>{
+        updatedState(true);
+    }
+    
     return (
     <div className="new-expense">
-    <ExpenseForm addForm={addFormHandler}></ExpenseForm>
+        {
+            (init)?
+            (<button onClick={onClick}>Add Expense</button>):
+            (<ExpenseForm addForm={addFormHandler} cancelhandler={cancelhandled}/>)
+          
+        }
+    
     </div>
     );
 }
